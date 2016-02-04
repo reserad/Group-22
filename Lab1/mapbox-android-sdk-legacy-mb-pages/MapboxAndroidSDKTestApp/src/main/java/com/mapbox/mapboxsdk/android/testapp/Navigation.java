d8eb0208ad510e9894819af6902760f7c3539e01
Alec Reser <reser13@gmail.com>
2016-02-04 12:19:15 -0500

    Transferring code from laptop to desktop

--- Lab1/mapbox-android-sdk-legacy-mb-pages/MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/Navigation.java
+++ Lab1/mapbox-android-sdk-legacy-mb-pages/MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/Navigation.java
@@ -2,10 +2,7 @@ package com.mapbox.mapboxsdk.android.testapp;
 
 import android.content.Context;
 import android.location.Address;
-import android.location.Criteria;
 import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -14,8 +11,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import com.mapbox.mapboxsdk.android.testapp.ui.CustomInfoWindow;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.overlay.Marker;
 import com.mapbox.mapboxsdk.views.MapView;
@@ -35,7 +33,6 @@ public class Navigation extends Fragment implements TabLayout.OnTabSelectedListe
         Button search = (Button)view.findViewById(R.id.search);
         final EditText input = (EditText)view.findViewById(R.id.input);
         mapView = (MapView) view.findViewById(R.id.mapview);
-        //final TextView navigateHere = (TextView)view.findViewById(R.id.tooltip_navigateHere);
 
         search.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View arg0) {
@@ -44,6 +41,7 @@ public class Navigation extends Fragment implements TabLayout.OnTabSelectedListe
                 try {
                     List<Address> addresses = geocoder.getFromLocation(lat.getLatitude(), lat.getLongitude(), 1);
                     Marker marker = new Marker(mapView, addresses.get(0).getAddressLine(0), addresses.get(0).getLocality() + ", " + addresses.get(0).getAdminArea(), lat);
+                    marker.setToolTip(new CustomInfoWindow(mapView));
                     mapView.addMarker(marker);
                     mapView.setCenter(lat);
                     mapView.setZoom(14);
@@ -53,23 +51,6 @@ public class Navigation extends Fragment implements TabLayout.OnTabSelectedListe
                 }
             }
         });
-/*
-        navigateHere.setOnClickListener(new TextView.OnClickListener() {
-            public void onClick(View arg0) {
-                Geocoder geocoder = new Geocoder(view.getContext(), Locale.getDefault());
-                LocationManager service = (LocationManager)     getActivity().getSystemService(getActivity().LOCATION_SERVICE);
-                Criteria criteria = new Criteria();
-                String provider = service.getBestProvider(criteria, false);
-                Location location = service.getLastKnownLocation(provider);
-                LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
-                try {
-                    List<Address> addresses = geocoder.getFromLocation(userLocation.getLatitude(), userLocation.getLongitude(), 1);
-                    mapView.addMarker(new Marker(mapView, addresses.get(0).getAddressLine(0), addresses.get(0).getLocality() + ", " + addresses.get(0).getAdminArea(), userLocation));
-                }
-                catch(IOException e) {
-                    Toast.makeText(view.getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }});*/
 
         mapView.setVisibility(View.VISIBLE);
         return view;
--- Lab1/mapbox-android-sdk-legacy-mb-pages/MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/ui/CustomInfoWindow.java
+++ Lab1/mapbox-android-sdk-legacy-mb-pages/MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/ui/CustomInfoWindow.java
@@ -1,17 +1,33 @@
 package com.mapbox.mapboxsdk.android.testapp.ui;
 
+import android.content.Context;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.location.LocationListener;
+
+import com.mapbox.mapboxsdk.android.testapp.Navigation;
 import com.mapbox.mapboxsdk.android.testapp.R;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.overlay.GpsLocationProvider;
 import com.mapbox.mapboxsdk.overlay.Marker;
 import com.mapbox.mapboxsdk.views.InfoWindow;
 import com.mapbox.mapboxsdk.views.MapView;
+import com.mapbox.mapboxsdk.overlay.UserLocationOverlay;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class CustomInfoWindow extends InfoWindow {
     
-    public CustomInfoWindow(MapView mv) {
+    public CustomInfoWindow(final MapView mv) {
         super(R.layout.infowindow_custom, mv);
         
         // Add own OnTouchListener to customize handling InfoWindow touch events
@@ -19,13 +35,27 @@ public class CustomInfoWindow extends InfoWindow {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() == MotionEvent.ACTION_UP) {
-                    // Demonstrate custom onTouch() control
-                    Toast.makeText(mView.getContext(), R.string.customInfoWindowOnTouchMessage, Toast.LENGTH_SHORT).show();
+                    Toast.makeText(v.getContext(), "hit", Toast.LENGTH_SHORT).show();
+                    UserLocationOverlay userLocation = new UserLocationOverlay(new GpsLocationProvider(v.getContext()), mv);
+                    mv.addMarker(new Marker(mv, "", "", userLocation.getMyLocation()));
 
-                    // Still close the InfoWindow though
+                    /*Geocoder geocoder = new Geocoder(v.getContext(), Locale.getDefault());
+                    LocationManager service = (LocationManager) v.getContext().getSystemService(Context.LOCATION_SERVICE);
+                    Criteria criteria = new Criteria();
+                    String provider = service.getBestProvider(criteria, false);
+                    service.requestLocationUpdates(provider, 1000, 0, mLocationListener);
+                    Location location = service.getLastKnownLocation(provider);
+
+                    LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    try {
+                        List<Address> addresses = geocoder.getFromLocation(userLocation.getLatitude(), userLocation.getLongitude(), 1);
+                        mv.addMarker(new Marker(mv, addresses.get(0).getAddressLine(0), addresses.get(0).getLocality() + ", " + addresses.get(0).getAdminArea(), userLocation));
+                    } catch (IOException e) {
+                        Toast.makeText(v.getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }*/
+
                     close();
                 }
-
                 // Return true as we're done processing this event
                 return true;
             }
@@ -40,5 +70,30 @@ public class CustomInfoWindow extends InfoWindow {
     public void onOpen(Marker overlayItem) {
         String title = overlayItem.getTitle();
         ((TextView) mView.findViewById(R.id.customTooltip_title)).setText(title);
+
+        String description = overlayItem.getDescription();
+        ((TextView) mView.findViewById(R.id.customTooltip_Description)).setText(description);
     }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 }
